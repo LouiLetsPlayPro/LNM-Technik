d44229bde242f907bcebdb1d8143364f37b805fe
LouiLetsPlayPro <51512114+LouiLetsPlayPro@users.noreply.github.com>
2022-07-16 21:21:34 +0200

    Create newmessage.js

---
+++ modulfiles/newmessage.js
@@ -0,0 +1,24 @@
+/**
+ * Dieses Modul wurde für das Projekt LNM Technik entwickelt!
+ * Es ist eine Erweiterung der allgemeinen Konsole.
+ * 
+ * Bitte dieses Modul nicht ohne Namensnennung verwenden!
+ * By Loui
+ */
+
+const fs = require('fs')
+const { console } = require('./console')
+
+module.exports = class {
+
+    /**
+     * @param {json} req
+     * @param {Express.Response} res 
+     */
+
+    static newmessage(req, res) {
+
+
+    }
+
+}
